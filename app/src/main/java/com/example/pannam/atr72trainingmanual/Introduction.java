15a8d6ee184a31babc8f68d12caa8bf5be8ba650
jesussmile <pullbackwild@gmail.com>
2016-03-12 16:47:47 +0545

    Added ViewPager & TabLayout

--- app/src/main/java/com/example/pannam/atr72trainingmanual/Introduction.java
+++ app/src/main/java/com/example/pannam/atr72trainingmanual/Introduction.java
@@ -6,15 +6,44 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 /**
  * Created by pannam on 3/2/2016.
  */
-public class Introduction extends Fragment{
+public class Introduction extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
 
-        return inflater.inflate(R.layout.introduction,container,false);
+
+        final RelativeLayout relativeLayout = new RelativeLayout(getActivity());
+        RelativeLayout.LayoutParams layoutParams = new RelativeLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+        relativeLayout.setLayoutParams(layoutParams);
+
+        RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT);
+
+        final ImageView introImage = new ImageView(getActivity());
+        introImage.setId(R.id.introImageId);
+        introImage.setImageResource(R.drawable.introduction);
+
+        introImage.setAnimation(AnimationUtils.loadAnimation(this.getContext(), android.R.anim.slide_out_right));
+
+        introImage.setPadding(100, 100, 100, 100);
+
+        final TextView introText = new TextView(getActivity());
+        introText.setText(R.string.introduction_text);
+        params.addRule(RelativeLayout.BELOW, introImage.getId());
+        relativeLayout.addView(introText,params);
+        relativeLayout.addView(introImage);
+
+
+        return relativeLayout;
+
     }
 }
